7e7d3febc35e4cd21f7a257e3c4e94462cd96a19
YotioSoft <ytani0323@gmail.com>
2023-06-02 00:50:49 +0900

    add hamming window func

--- gabor.py
+++ gabor.py
@@ -3,3 +3,12 @@ def DGT(x, w, L, a, b, m, n):
     for l in range(L):
         X += x[l] * w[l - a * n] * np.exp((-2 * np.pi * 1j * b * m * l) / L)
     return X
+
+def hammig_w(t):
+    return 0.54 - 0.46 * np.cos((2 * np.pi * t) / T)
+
+def hamming(T):
+    W = np.zeros(T)
+    for t in range(T):
+        W[t] = 0.54 - 0.46 * np.cos((2 * np.pi * t) / T)
+    return W
