26bbe5d62e08bfc316db20ae652fbb6cee946572
YotioSoft <ytani0323@gmail.com>
2023-06-01 20:35:42 +0900

    gabor -> DGT

--- gabor.py
+++ gabor.py
@@ -1,7 +1,5 @@
-def gabor(x, w, L, m, n):
+def DGT(x, w, L, a, b, m, n):
     X = 0
     for l in range(L):
-        X += x[l] * w[l - n] * np.exp((-2 * np.pi * 1j * m * l) / L)
+        X += x[l] * w[l - a * n] * np.exp((-2 * np.pi * 1j * b * m * l) / L)
     return X
-
-
