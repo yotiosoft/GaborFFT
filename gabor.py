b6ff16f79cca3a6a033e3a230276daeafc2c5f27
YotioSoft <ytani0323@gmail.com>
2023-06-07 12:11:44 +0900

    use dtype=complex

--- gabor.py
+++ gabor.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 T = 100
 a = 1
@@ -8,10 +9,10 @@ N = int(L / a)
 M = int(L / b)
 
 def DGT(x, w, a, b, m, n):
-    X = 0
+    dgt_X = 0
     for l in range(L):
-        X += x[l] * w[l - a * n] * np.exp((-2 * np.pi * 1j * b * m * l) / L)
-    return X
+        dgt_X += x[l] * w[l - a * n] * np.exp((-2 * np.pi * 1j * b * m * l) / L)
+    return dgt_X
 
 def hammig_w(t):
     return 0.54 - 0.46 * np.cos((2 * np.pi * t) / T)
@@ -24,8 +25,10 @@ x = np.zeros(L)
 for l in range(L):
     x[l] = np.sin(np.pi * l)
 
-X = np.zeros((M, N))
+X = np.zeros((M, N), dtype=complex)
 for m in range(M):
     for n in range(N):
         X[m, n] = DGT(x, w, a, b, m, n)
         print("m:" + str(m) + ", n:" + str(n) + " : " + str(X[m, n]))
+
+plt.plot(1,1,marker='.')
