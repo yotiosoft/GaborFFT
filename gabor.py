ddd888bb112d38be7829d6ab06b453579702e23c
YotioSoft <ytani0323@gmail.com>
2023-06-07 20:58:34 +0900

    fix DGT window -> pass sin test

--- gabor.py
+++ gabor.py
@@ -1,29 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.io.wavfile as wio
 
 T = 100
-a = 10
-b = 10
-L = 1000
+a = 100
+b = 100
+L = 10000
 N = int(L / a)
 M = int(L / b)
 
 def DGT(x, w, a, b, m, n):
     dgt_X = 0
     for l in range(L):
-        dgt_X += x[l] * w[l - a * n] * np.exp((-2 * np.pi * 1j * b * m * l) / L)
+        dgt_X += x[l] * w[(l - a * n) % T] * np.exp((-2 * np.pi * 1j * b * m * l) / L)
     return dgt_X
 
 def hammig_w(t):
     return 0.54 - 0.46 * np.cos((2 * np.pi * t) / T)
 
-w = np.zeros(L)
+w = np.zeros(T)
 for t in range(T):
     w[t] = hammig_w(t)
 
+# sample: sin
 x = np.zeros(L)
 for l in range(L):
     x[l] = np.sin(np.pi * l)
+
+# sample: wav
+#fs, x = wio.read("BabyElephantWalk60.wav")
+#L = len(x)
+#N = int(L / a)
+#M = int(L / b)
+#pxx, freq, bins, t = plt.specgram(x,Fs = fs)
+#plt.show()
 
 X = np.zeros((M, N), dtype=complex)
 for m in range(M):
