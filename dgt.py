b9745947219a6678da04c586d6e43562298c0c5e
YotioSoft <ytani0323@gmail.com>
2023-08-10 12:43:19 +0900

    temporaly classify

--- dgt.py
+++ dgt.py
@@ -9,224 +9,208 @@ import time
 
 THREADS = 8
 
-T = 500
-b = 50
-a = 50
-START = 5000
-END = 20000
-L = END - START
-M = int(L / b)  # y
-N = int(L / a)  # x
+class DGT:
+    def __init__(self, t, a, b, start, end):
+        self.T = t
+        self.a = a
+        self.b = b
+        self.START = start
+        self.END = end
+        self.L = end - start
+        self.M = int(self.L / self.b)  # y
+        self.N = int(self.L / self.a)  # x
 
-def DGT(x, w, m, n):
-    dgt_X = 0
-    for l in range(a * n, a * n + T):
-        if l >= L:
-            break
-        dgt_X += x[l] * w[l - a * n] * np.exp((-2 * np.pi * 1j * m * l) / complex(M))
-    return dgt_X
-
-def IDGT(X, g, w, l):
-    idgt_x = 0
-    #print("l:" + str(l) + ", g[l][l]:" + str(g[l][l]))
-    for n in range(N):
-        if l - a * n < 0 or l - a * n >= T:
-            continue
-        for m in range(M):
-            idgt_x += X[m, n] * g[l - a * n] * np.exp((2 * np.pi * 1j * m * l) / complex(M))
+    def calc_DGT(self, x, w, m, n):
+        dgt_X = 0
+        for l in range(self.a * n, self.a * n + self.T):
+            if l >= self.L:
+                break
+            dgt_X += x[l] * w[l - self.a * n] * np.exp((-2 * np.pi * 1j * m * l) / complex(self.M))
+        return dgt_X
     
-    return idgt_x
+    def calc_IDGT(self, X, g, w, l):
+        idgt_x = 0
+        #print("l:" + str(l) + ", g[l][l]:" + str(g[l][l]))
+        for n in range(self.N):
+            if l - self.a * n < 0 or l - self.a * n >= self.T:
+                continue
+            for m in range(self.M):
+                idgt_x += X[m, n] * g[l - self.a * n] * np.exp((2 * np.pi * 1j * m * l) / complex(self.M))
+        
+        return idgt_x
+    
+    def hammig_w(self):
+        w = np.zeros(self.T)
+        for t in range(self.T):
+            w[t] = 0.54 - 0.46 * np.cos((2 * np.pi * t) / self.T)
 
-def hammig_w():
-    w = np.zeros(T)
-    for t in range(T):
-        w[t] = 0.54 - 0.46 * np.cos((2 * np.pi * t) / T)
+        return w
+    
+    def calc_X(self, x, w, m0, m1, n0, n1):
+        temp_X = np.zeros((self.M, self.N), dtype=complex)
+        for m in range(m0, m1):
+            for n in range(n0, n1):
+                temp_X[m, n] = self.calc_DGT(x, w, m, n)
+                # print("m:" + str(m) + ", n:" + str(n) + " : " + str(temp_X[m, n]))
+        return (temp_X, m0, m1, n0, n1)
+    
+    def calc_cx(self, X, cw, w, l0, l1):
+        temp_cx = np.zeros(self.L, dtype=complex)
+        for l in range(l0, l1):
+            temp_cx[l] = self.calc_IDGT(X, cw, w, l)
+        return (temp_cx, l0, l1)
+    
+    def hammig_cw(self, w):
+        sum = np.zeros(self.L)
+        for l in range(self.L):
+            sum[l] = 0
+            for n in range(self.N):
+                if l == 10 and n < 10:
+                    nw = np.zeros(self.L)
+                    nw[l+self.a*n:l+self.a*n+self.T] = w
+                    plt.plot(nw)
+                sum[l] += np.abs(w[(l + self.a * n) % self.T]) ** 2
+        plt.xlim(0, 1000)
+        plt.show()
 
-    return w
+        plt.plot(sum)
+        plt.show()
 
-def hammig_cw(w, a):
-    """
-    cw_a = np.zeros(a)
-    for l in range(a):
-        for n in range(N):
-            cw_a[l] += np.abs(w[l]) ** 2
+        plt.cla()
 
-    cw = np.zeros((L, L), dtype=complex)
-    for l in range(l):
-        cw[l][l] = cw_a[l]
-    """
-    sum = np.zeros(L)
-    for l in range(L):
-        sum[l] = 0
-        for n in range(N):
-            if l == 10 and n < 10:
-                nw = np.zeros(L)
-                nw[l+a*n:l+a*n+T] = w
-                plt.plot(nw)
-            sum[l] += np.abs(w[(l + a * n) % T]) ** 2
-    plt.xlim(0, 1000)
-    plt.show()
+        cw_a = np.zeros((self.a, self.a), dtype=complex)
+        for l in range(self.a):
+            for n in range(self.N):
+                cw_a[l][l] += np.abs(w[(l + self.a * n) % self.T]) ** 2
+            cw_a[l][l] *= self.M
+            cw_a[l][l] = 1 / (np.dot(cw_a[l][l].T, cw_a[l][l]))
 
-    plt.plot(sum)
-    plt.show()
+        cw = np.zeros((self.L, self.L), dtype=complex)
+        for l in range(self.L):
+            cw[l][l] = cw_a[l % self.a][l % self.a]
 
-    plt.cla()
+        g = np.zeros(self.T)
+        for t in range(self.T):
+            g[t] = cw[t][t] * w[t]
 
-    cw_a = np.zeros((a, a), dtype=complex)
-    for l in range(a):
-        for n in range(N):
-            cw_a[l][l] += np.abs(w[(l + a * n) % T]) ** 2
-        cw_a[l][l] *= M
-        cw_a[l][l] = 1 / (np.dot(cw_a[l][l].T, cw_a[l][l]))
+        plt.plot(g)
+        plt.show()
 
-    cw = np.zeros((L, L), dtype=complex)
-    for l in range(L):
-        cw[l][l] = cw_a[l % a][l % a]
+        return g
+    
+    def db(self, x, dBref):
+        y = 20 * np.log10(x / dBref)                   # 変換式
+        return y                                       # dB値を返す
+    
+    def dgt(self):
+        w = self.hammig_w()
+        cw = self.hammig_cw(w)
 
-    g = np.zeros(T)
-    for t in range(T):
-        g[t] = cw[t][t] * w[t]
+        t = np.linspace(0,self. L, self.L)
 
-    plt.plot(g)
-    plt.show()
+        fs, x = wio.read("MSK.20100405.M.CS05.wav")
+        x = x[self.START:self.END]
+        print(x)
+        print("Length: " + str(len(x)))
+        print("fs: " + str(fs))
+        if len(x) > self.L:
+            x = x[0:self.L]
+        elif len(x) < self.L:
+            x = np.append(x, np.zeros(self.L - len(x)))
 
-    return g
+        # 解析
+        X = np.zeros((self.M, self.N), dtype=complex)
+        future_list = []
+        start = time.time()
+        prev_m1 = 0
+        max_m = self.M
+        with ThreadPoolExecutor(max_workers=8) as e:
+            for i in range(THREADS):
+                m0 = (int)(i * (max_m / THREADS))
+                if i == THREADS - 1:
+                    m1 = max_m
+                else:
+                    m1 = max(prev_m1 + 1, (int)((i + 1) * (max_m / THREADS)))
+                prev_m1 = m1
+                n0 = 0
+                n1 = self.N
+                print("m0:" + str(m0) + ", m1:" + str(m1) + ", n0:" + str(n0) + ", n1:" + str(n1))
+                future = e.submit(self.calc_X, x, w, m0, m1, n0, n1)
+                future_list.append(future)
 
-def calc_X(x, w, m0, m1, n0, n1):
-    temp_X = np.zeros((M, N), dtype=complex)
-    for m in range(m0, m1):
-        for n in range(n0, n1):
-            temp_X[m, n] = DGT(x, w, m, n)
-            # print("m:" + str(m) + ", n:" + str(n) + " : " + str(temp_X[m, n]))
-    return (temp_X, m0, m1, n0, n1)
+            i = 0
+            for future in futures.as_completed(fs=future_list):
+                temp_X, m0, m1, n0, n1 = future.result()
+                print("complete: " + str(m0) + ":" + str(m1) + ", " + str(n0) + ":" + str(n1))
+                X[m0:m1, n0:n1] = temp_X[m0:m1, n0:n1]
+                i += 1
+        print ("time: " + str(time.time()-start))
 
-def calc_cx(X, cw, w, l0, l1):
-    temp_cx = np.zeros(L, dtype=complex)
-    for l in range(l0, l1):
-        temp_cx[l] = IDGT(X, cw, w, l)
-    return (temp_cx, l0, l1)
+        # 逆変換
+        start = time.time()
+        cx = np.zeros(self.L, dtype=complex)
+        future_list = []
+        with ThreadPoolExecutor(max_workers=8) as e:
+            for i in range(THREADS):
+                l0 = (int)(i * (self.L / THREADS))
+                if i == THREADS - 1:
+                    l1 = self.L
+                else:
+                    l1 = (int)((i + 1) * (self.L / THREADS))
+                print("l0:" + str(l0) + ", l1:" + str(l1))
+                future = e.submit(self.calc_cx, X, cw, w, l0, l1)
+                future_list.append(future)
 
-def db(x, dBref):
-    y = 20 * np.log10(x / dBref)                   # 変換式
-    return y                                       # dB値を返す
+            i = 0
+            for future in futures.as_completed(fs=future_list):
+                temp_cx, l0, l1 = future.result()
+                print("complete: " + str(i))
+                cx[l0:l1] = temp_cx[l0:l1]
+                i += 1
+        print ("time: " + str(time.time()-start))
+        print("cx:")
+        print(cx)
 
-w = hammig_w()
+        # 逆変換結果をwavファイルに出力
+        wio.write("output.wav", fs, np.real(cx))
 
-cw = hammig_cw(w, a)
+        # spectrogram
+        fig1, ax1 = plt.subplots()
+        fig2, ax2 = plt.subplots()
+        fig3, ax3 = plt.subplots()
+        fig4, ax4 = plt.subplots()
+        fig5, ax5 = plt.subplots()
+        fig6, ax6 = plt.subplots()
 
-RT = L
-t = np.linspace(0, L, L)
-#x = np.sin(100*np.pi*5*t)
-# xの残りの部分を0埋め
-#x = np.append(x, np.zeros(L - RT))
+        # プロット用パラメータ
+        x_max = self.L
+        y_max = self.L / 2
+        X = X[0:(int)(y_max/self.b), 0:(int)(x_max/self.a)]
+        print(len(X))
+        print(np.linspace(0, y_max, self.N))
 
-# sample: wav
-#fs, x = wio.read("shining.wav")
-#x = x[:, 0]
-fs, x = wio.read("MSK.20100405.M.CS05.wav")
-#x = x[:, 0]
-x = x[START:END]
-print(x)
-print("Length: " + str(len(x)))
-print("fs: " + str(fs))
-if len(x) > L:
-    x = x[0:L]
-elif len(x) < L:
-    x = np.append(x, np.zeros(L - len(x)))
+        # decibelize
+        X2 = self.db(X, 2e-5)
 
-#pxx, freq, bins, t = plt.specgram(x,Fs = fs)
-#plt.show()
+        # 波形
+        ax1.plot(t, x)
 
-# 解析
-X = np.zeros((M, N), dtype=complex)
-future_list = []
-start = time.time()
-prev_m1 = 0
-max_m = M
-with ThreadPoolExecutor(max_workers=8) as e:
-    for i in range(THREADS):
-        m0 = (int)(i * (max_m / THREADS))
-        if i == THREADS - 1:
-            m1 = max_m
-        else:
-            m1 = max(prev_m1 + 1, (int)((i + 1) * (max_m / THREADS)))
-        prev_m1 = m1
-        n0 = 0
-        n1 = N
-        print("m0:" + str(m0) + ", m1:" + str(m1) + ", n0:" + str(n0) + ", n1:" + str(n1))
-        future = e.submit(calc_X, x, w, m0, m1, n0, n1)
-        future_list.append(future)
+        # 1秒分の波形
+        ax2.stem(t, x, '*')
+        ax2.set_xlim(0, 100)
 
-    i = 0
-    for future in futures.as_completed(fs=future_list):
-        temp_X, m0, m1, n0, n1 = future.result()
-        print("complete: " + str(m0) + ":" + str(m1) + ", " + str(n0) + ":" + str(n1))
-        X[m0:m1, n0:n1] = temp_X[m0:m1, n0:n1]
-        i += 1
-print ("time: " + str(time.time()-start))
+        # 窓
+        ax3.plot(w)
 
-# 逆変換
-start = time.time()
-cx = np.zeros(L, dtype=complex)
-future_list = []
-with ThreadPoolExecutor(max_workers=8) as e:
-    for i in range(THREADS):
-        l0 = (int)(i * (L / THREADS))
-        if i == THREADS - 1:
-            l1 = L
-        else:
-            l1 = (int)((i + 1) * (L / THREADS))
-        print("l0:" + str(l0) + ", l1:" + str(l1))
-        future = e.submit(calc_cx, X, cw, w, l0, l1)
-        future_list.append(future)
+        # 解析結果
+        c = ax5.contourf(np.linspace(0, x_max, (int)(x_max/self.a)), np.linspace(0, y_max, (int)(y_max/self.b)), np.abs(X2), 50, cmap='jet')
+        c = ax4.contourf(np.linspace(0, x_max, (int)(x_max/self.a)), np.linspace(0, y_max, (int)(y_max/self.b)), np.abs(X), 20, locator=ticker.LogLocator(), cmap='jet')
+        fig4.colorbar(c)
 
-    i = 0
-    for future in futures.as_completed(fs=future_list):
-        temp_cx, l0, l1 = future.result()
-        print("complete: " + str(i))
-        cx[l0:l1] = temp_cx[l0:l1]
-        i += 1
-print ("time: " + str(time.time()-start))
-print("cx:")
-print(cx)
+        # 逆変換結果
+        ax6.plot(t, cx)
 
-# 逆変換結果をwavファイルに出力
-wio.write("output.wav", fs, np.real(cx))
+        plt.show()
 
-# spectrogram
-fig1, ax1 = plt.subplots()
-fig2, ax2 = plt.subplots()
-fig3, ax3 = plt.subplots()
-fig4, ax4 = plt.subplots()
-fig5, ax5 = plt.subplots()
-fig6, ax6 = plt.subplots()
-
-# プロット用パラメータ
-x_max = L
-y_max = L / 2
-X = X[0:(int)(y_max/b), 0:(int)(x_max/a)]
-print(len(X))
-print(np.linspace(0, y_max, N))
-
-# decibelize
-X2 = db(X, 2e-5)
-
-# 波形
-ax1.plot(t, x)
-
-# 1秒分の波形
-ax2.stem(t, x, '*')
-ax2.set_xlim(0, 100)
-
-# 窓
-ax3.plot(w)
-
-# 解析結果
-c = ax5.contourf(np.linspace(0, x_max, (int)(x_max/a)), np.linspace(0, y_max, (int)(y_max/b)), np.abs(X2), 50, cmap='jet')
-c = ax4.contourf(np.linspace(0, x_max, (int)(x_max/a)), np.linspace(0, y_max, (int)(y_max/b)), np.abs(X), 20, locator=ticker.LogLocator(), cmap='jet')
-fig4.colorbar(c)
-
-# 逆変換結果
-ax6.plot(t, cx)
-
-plt.show()
+dgt = DGT(500, 50, 50, 5000, 20000)
+dgt.dgt()
