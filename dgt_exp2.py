dfb7326f7af9a70b544f7aeeee411b92dc36dbd1
YotioSoft <ytani0323@gmail.com>
2023-08-12 20:24:06 +0900

    fix exp2

--- dgt_exp2.py
+++ dgt_exp2.py
@@ -7,11 +7,13 @@ import sys
 
 # sample: wav
 w = mydgt.hammig_w(500)
+plt.plot(w)
+plt.show()
 
 a = 50
-b = 50
-start = 0
-end = sys.maxsize    # ファイル全体
+b = 25
+start = 5000
+end = 20000
 
 # 音声を読み込み
 x, fs = mydgt.load_wav("MSK.20100405.M.CS05.wav", start, end)
@@ -23,13 +25,14 @@ M = int(L / b)
 # 窓関数wに時間変数tで重み付け
 t = np.arange(-len(w)/2, len(w)/2)
 tw = w * t
-
-plt.plot(t, tw)
+plt.plot(tw)
 plt.show()
 
 # 窓関数wを時間微分
 dw = np.diff(w)
 print(dw)
+plt.plot(dw)
+plt.show()
 
 # 音声を順変換
 dgt1 = mydgt.DGT(len(w), a, b, L)
@@ -40,13 +43,15 @@ dgt3 = mydgt.DGT(len(dw), a, b, L)
 X_dwdt = dgt3.dgt(x, dw)
 
 # 再割当て法
-new_X = np.zeros((N*5, M*5), dtype=np.complex)
-for n in range(M):
-    for m in range(N):
+new_X = np.zeros((M, N), dtype=np.complex)
+for m in range(M):
+    for n in range(N):
         n_new = n + (np.abs((X_tw[m, n] / X[m, n]).real) / a)
         m_new = m - (np.abs((X_dwdt[m, n] / X[m, n]).imag) / b)
         print(n_new, m_new)
-        new_X[int(m_new), int(n_new)] = X[m, n]
+        if int(n_new) >= N or int(m_new) >= M:
+            continue
+        new_X[int(m_new), int(n_new)] += X[m, n]
 
 
 # 合成した上で逆変換
